2803f26a8177198437e60c208e98f0f0a30e1cb9
Lucas Eckman <lucas.eckman@gmail.com>
2018-03-08 12:21:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,7 +4,10 @@ var playlist = {
 
 function updatePlaylist(obj, key, value) {
   obj[key] = value
-  
   return obj
 }
 
+function removeFromPlaylist(obj, key) {
+  delete obj.key;
+  return obj
+}
